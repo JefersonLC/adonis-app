c1dc89e4b4b109e67ee1c3c2f5f4ce5cc478cf59
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-24 19:21:28 -0500

    added new routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,18 @@
 import Route from '@ioc:Adonis/Core/Route'
 // Home
-Route.on('/').render('home').as('home').middleware('checkAuth')
+Route.on('/').render('home').as('home').middleware(['checkAuth', 'verifyEmail'])
 
 // Products
-Route.on('/products').render('products').as('products').middleware('checkAuth')
+Route.on('/products').render('products').as('products').middleware(['checkAuth', 'verifyEmail'])
 
 // Auth
 Route.on('/login').render('login').as('login.form')
 
 Route.post('/login', 'AuthController.login').as('user.login')
 
-Route.get('/logout', 'AuthController.logout').as('user.logout').middleware('auth')
+Route.get('/logout', 'AuthController.logout').as('user.logout').middleware(['auth', 'verifyEmail'])
+
+Route.get('/verify-email', 'AuthController.verifyEmail')
 
 // Register
 Route.on('/register').render('register').as('register.form')
@@ -18,4 +20,4 @@ Route.on('/register').render('register').as('register.form')
 Route.post('/register', 'UsersController.create').as('user.create')
 
 // Profile
-Route.on('/profile').render('profile').as('user.profile').middleware('auth')
+Route.on('/profile').render('profile').as('user.profile').middleware(['auth', 'verifyEmail'])
