8e52f8ab56dbe7c3e9f3399b438ff0082eba956d
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-25 22:43:30 -0500

    added routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,33 @@
 import Route from '@ioc:Adonis/Core/Route'
+
 // Home
 Route.on('/').render('home').as('home').middleware(['checkAuth', 'verifyEmail'])
 
+// Categories
+Route.get('/api/categories', 'CategoriesController.panel').middleware([
+  'auth',
+  'verifyEmail',
+  'isAdmin',
+])
+
+Route.post('/api/categories', 'CategoriesController.create').middleware([
+  'auth',
+  'verifyEmail',
+  'isAdmin',
+])
+
 // Products
 Route.on('/products').render('products').as('products').middleware(['checkAuth', 'verifyEmail'])
+
+Route.get('/api/products', 'ProductsController.panel').middleware([
+  'auth',
+  'verifyEmail',
+  'isAdmin',
+])
+
+Route.post('/products', 'ProductsController.create')
+  .as('product.create')
+  .middleware(['auth', 'verifyEmail', 'isAdmin'])
 
 // Auth
 Route.on('/login').render('login').as('login.form')
@@ -21,3 +45,9 @@ Route.post('/register', 'UsersController.create').as('user.create')
 
 // Profile
 Route.on('/profile').render('profile').as('user.profile').middleware(['auth', 'verifyEmail'])
+
+// Control-panel
+Route.on('/control-panel')
+  .render('control-panel')
+  .as('control.panel')
+  .middleware(['auth', 'verifyEmail', 'isAdmin'])
